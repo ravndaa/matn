2186201fc3b8dde2fec906dee26c8de7e0b701e6
Stian Ravndal <stian.ravndal@outlook.com>
2022-09-27 10:36:22 +0200

    more changes

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,7 +1,9 @@
+// #pragma warning disable CA1852
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var secretKey = "SuperSecretKEY"; //pragma: allowlist secret
+//var secretKey = "SuperSecretKEY"; //pragma: allowlist secret
 
 app.MapGet("/", () => "Hello World!");
 
