5ca2829dc5176fa8916fdc130bb8b17796a65133
Stian Ravndal <stian.ravndal@outlook.com>
2022-09-27 11:15:52 +0200

    does this work

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // this should fail when running actions.
-var secretKey = "SuperSecretEY"; //pragma: allowlist secret
+// fixed issue =)
+// var secretKey = "SuperSecretEY"; //pragma: allowlist secret
 
 app.MapGet("/", () => "Hello World!");
 
