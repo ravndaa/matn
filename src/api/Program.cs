cecd388d10ff096347a4d820ce604381d2be9248
Stian Ravndal <stian.ravndal@outlook.com>
2022-09-27 11:05:03 +0200

    added comment

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -2,6 +2,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// this should fail when running actions.
 var secretKey = "SuperSecretEY"; //pragma: allowlist secret
 
 app.MapGet("/", () => "Hello World!");
